f965beba5c7cd8ea20857d8ae2c3ca003439b38b
MouedhSouabni1905 <mouedhsmsouabni@gmail.com>
2025-01-15 19:34:30 +0100

    finally uploaded my dotfiles

---
+++ install.sh
@@ -0,0 +1,6 @@
+mv i3conf $HOME/.config/i3/config
+mv launch_polybar.sh $HOME/.config/polybar/launch.sh
+mv polybar_config.ini $HOME/.config/polybar/config.ini
+mv picom.conf $HOME/.config/picom/picom.conf
+cd ../
+rm -rf dotfiles
